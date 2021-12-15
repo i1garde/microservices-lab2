3e0e975d7d6095de412522c5e65c29a3e11a2772
Ivan Komarenko <vanyakomarenko@gmail.com>
2021-12-16 00:01:23 +0200

    lab2 done

--- unhealthy-api/Program.cs
+++ unhealthy-api/Program.cs
@@ -1,16 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
 var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 
 var status = "OK";
 
-app.MapGet("/api/service2", () =>
+app.MapGet("/api/service2", async () =>
 {
     if (status != "OK")
     {
-        Thread.Sleep(10000);
+        await Lab.Wait();
     }
-    return $"Status: {status}\nHello World!";
+
+    Message message = new Message {Route = "/api/service2", Status = $"{status}", Responce = "Hello World!"};
+    return message;
 });
 
 app.MapGet("/api/service2/untested-request", () =>
@@ -20,3 +26,16 @@ app.MapGet("/api/service2/untested-request", () =>
 });
 
 app.Run();
+
+public class Message
+{
+    public string Route { get; set; }
+    
+    public string Status { get; set; }
+    public string Responce { get; set; }
+}
+
+public static class Lab
+{
+    public static async Task Wait() => await Task.Delay(10000);
+}
